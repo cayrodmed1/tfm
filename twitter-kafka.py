9d70934c9f12ba0834ce8be3382755c993be5d82
Cayetano Rodríguez <cayetanorodriguez94@gmail.com>
2017-06-05 19:41:05 +0200

    Update twitter-kafka.py

--- twitter-kafka.py
+++ twitter-kafka.py
@@ -5,15 +5,15 @@ import string
 from pykafka import KafkaClient
 
 # Twitter API tokens
-ACCESS_TOKEN = '381520903-OwPTkjllzq7T9cmeuloBQzUVUBxjjjD3c3YjBB18'
-ACCESS_SECRET = 'lOBE3wrQZWuPvDoiF4vhfhCZB3oiRbFTArdVmf2CZDBqC'
-CONSUMER_KEY = 'KLRKa701PFkAboW9wMhACOa1x'
-CONSUMER_SECRET = 'OlREM6mGWSy7JvBEhjl4mhtJPoNvc7G4wcr2ptKEuzGX0s68yH'
+ACCESS_TOKEN = 'XXXXXXXXXXXXXXXXXXX'
+ACCESS_SECRET = 'XXXXXXXXXXXXXXXXXXXX'
+CONSUMER_KEY = 'XXXXXXXXXXXXXXXXXXXXXX'
+CONSUMER_SECRET = 'XXXXXXXXXXXXXXXXXXXXX'
 
 # Listener class to be used by tweepy
 class StdOutListener(tweepy.StreamListener):
 
-    # When a tweet comes, send attribute 'created_at' to Kafka
+    # When a tweet comes, send attributes 'created_at' and 'screen_name' to Kafka
     def on_status(self, status):
         date = str(status.created_at)
         user = status._json['user']['screen_name']
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     stream = tweepy.Stream(oauth, listener)
 
     # We need to specify the Kafka server
-    client = KafkaClient(hosts="ec2-52-29-143-245.eu-central-1.compute.amazonaws.com:9092")
+    client = KafkaClient(hosts="tfmkafka.ddns.net:9092")
     
     # This is the topic in the Kafka broker
     mytopic = client.topics['twitter']
